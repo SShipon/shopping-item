bef8eacd89dbdb9fdfaa8d7283b828e66d3807ef
SShipon <shamsulislamshipon2022@gmail.com>
2021-09-08 11:47:20 +0600

    hello

--- home.js
+++ home.js
@@ -1,6 +1,16 @@
+const displayStorageCart = () =>{
+    const cart = getCard();
+    for(const name  in cart ){
+        displayProduct(name);
+    }
+}
 const addProduct = () =>{
 const nameField = document.getElementById('product-name');
 const name = nameField.value ;
+if (!name) {
+    return;
+    
+}
 displayProduct(name)
 addProductToCart(name)
 
@@ -27,7 +37,18 @@ const getCard = () =>{
 }
 const addProductToCart = name =>{
     const cart = getCard();
-    cart[name] = 1;
+    if(cart[name]){
+        cart[name] = cart [name]+1;
+    }
+    else{
+      cart[name] = 1;  
+    }
     const carsStringify = JSON.stringify(cart);
     localStorage.setItem('cart',carsStringify);
 }
+
+const placeOrder =() =>{
+    document.getElementById('products').textContent ='';
+    localStorage.removeItem('cart');
+}
+displayStorageCart()
